4461b5cd515c54e5441ffaca3222ddc34a61dd41
Ekaterina Shreyner <kkkatya88@yandex.ru>
2023-05-25 17:17:53 +0300

    Rename components Main Header Footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 // import React from "react";
 import { useState } from "react";
-import Header from "./Header.js";
-import Footer from "./Footer.js"
-import Main from "./Main.js";
+import Header from "./Header1.js";
+import Footer from "./Footer1.js";
+import Main from "./Main1.js";
 import PopupWithForm from "./PopupWithForm.js";
 import ImagePopup from "./ImagePopup.js";
 
